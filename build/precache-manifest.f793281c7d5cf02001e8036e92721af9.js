00daccf503791a1df79c4bb9521e935131f56ab7
albastardoto <albert.koppelmaa@hotmail.com>
2019-09-22 14:55:58 +0200

    added build folder

---
+++ build/precache-manifest.f793281c7d5cf02001e8036e92721af9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86a6ad6f19bfbd1ce5bd0c50716872a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2024d06dbcd031cb2925",
+    "url": "/static/css/main.822270b8.chunk.css"
+  },
+  {
+    "revision": "2aeac56659390a34f676",
+    "url": "/static/js/2.a59134a9.chunk.js"
+  },
+  {
+    "revision": "2024d06dbcd031cb2925",
+    "url": "/static/js/main.03086aab.chunk.js"
+  },
+  {
+    "revision": "e3ae7e0b27f7f22adfbe",
+    "url": "/static/js/runtime~main.39e88118.js"
+  }
+]);
