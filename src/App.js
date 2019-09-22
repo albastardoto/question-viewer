68ec6b1db9c12036088a1636688625f273a561f9
albastardoto <albert.koppelmaa@hotmail.com>
2019-09-22 14:48:29 +0200

    finished website for viewing questions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import questions from './data.json';
 
 function App() {
+  console.log(questions);
+  const idquestions = questions.map((question, index) => {
+    return {...question, id: index};
+  });
+  const [state, setState] = useState({
+    hidden: [],
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {idquestions
+        .filter((question, index) => !state.hidden.includes(index))
+        .map((question, index) => {
+          return (
+            <Question
+              number={question.id}
+              key={index}
+              name={question.question}
+              choices={question.choices}
+              image={question.image}
+              onDivClick={key => setState({hidden: [...state.hidden, key]})}
+            />
+          );
+        })}
     </div>
   );
 }
+const Question = props => {
+  return (
+    <div
+      onClick={props.onDivClick.bind(this, props.number)}
+      className="question">
+      <h2>{props.number + '.  ' + props.name}</h2>
+      {props.image && (
+        <img
+          alt={props.image}
+          className="questionImage"
+          src={'http://www2.snca.lu/scripts/lusitStpd/' + props.image}
+        />
+      )}
 
+      {props.choices.map((choice, index) => {
+        let className = 'choice';
+        if (choice.substring(0, 1) === '1') {
+          className += ' correctChoice';
+        }
+        return (
+          <h3 key={index} className={className}>
+            {choice.substring(2)}
+          </h3>
+        );
+      })}
+    </div>
+  );
+};
 export default App;
